cf9b94e96fc3382b0fd5d5df781c5389221c3be8
YOKESHDHANABALAN <107738665+YOKESHDHANABALAN@users.noreply.github.com>
2022-09-10 12:57:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+var countdown = document.getElementById("countdown");
+setTimeout(() =>{
+    countdown.innerText = "10";
+    console.log("10");
+},1000);
+
+setTimeout(() =>{
+    countdown.innerText = "9";
+    console.log("9");
+},2000);
+
+setTimeout(() =>{
+    countdown.innerText = "8";
+    console.log("8");
+},3000);
+
+setTimeout(() =>{
+    countdown.innerText = "7";
+    console.log("7");
+},4000);
+
+setTimeout(() =>{
+    countdown.innerText = "6";
+    console.log("6");
+},5000);
+
+
+setTimeout(() =>{
+    countdown.innerText = "5";
+    console.log("5");
+},6000);
+
+setTimeout(() =>{
+    countdown.innerText = "4";
+    console.log("4");
+},7000);
+
+setTimeout(() =>{
+    countdown.innerText = "3";
+    console.log("3");
+},8000);
+
+
+setTimeout(() =>{
+    countdown.innerText = "2";
+    console.log("2");
+},9000);
+
+
+setTimeout(() =>{
+    countdown.innerText = "1";
+    console.log("1");
+},10000);
+
+
+setTimeout(() =>{
+    countdown.innerText = " HAPPY INDEPENDENCE DAY ";
+    console.log(" HAPPY INDEPENDENCE DAY ");
+},11000);
